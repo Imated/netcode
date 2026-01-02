dfafc839ee9fc5f4eab6dc5fe6ec8b2471361e6f
Imated <lineborder122@gmail.com>
2026-01-02 02:10:08 -0500

    Started server things

---
+++ src/lib.rs
@@ -0,0 +1,4 @@
+﻿mod server;
+
+pub const PRIVATE_KEY_BYTES: usize = 32;
+pub type Key = [u8; PRIVATE_KEY_BYTES];
---
+++ src/server.rs
@@ -0,0 +1,19 @@
+﻿use crate::{Key, PRIVATE_KEY_BYTES};
+
+pub struct ServerConfig {
+    pub protocol_id: u64,
+    pub private_key: Key,
+    pub keep_alive_send_rate: f64,
+    pub client_timeout_secs: i32,
+}
+
+impl Default for ServerConfig {
+    fn default() -> Self {
+        Self {
+            keep_alive_send_rate: 0.1,
+            client_timeout_secs: 15,
+            protocol_id: 0,
+            private_key: [0; PRIVATE_KEY_BYTES],
+        }
+    }
+}
